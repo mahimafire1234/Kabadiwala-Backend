426a08b7ab632b2903d37c22d7974963db1081c9
rajivsunar <rajivsunar07@gmail.com>
2021-12-13 18:01:25 +0545

    User registration controller added

---
+++ controllers/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv")
+const fs = require('fs')
+
+const User = require("../models/user")
+
+exports.register = (req, res) => {
+
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Email address already used"
+                });
+            } else {
+                
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return error_message(res, err, "Registration failed")
+                    } else {
+                     
+                        const user = new User({
+                            _id: mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash,
+                            name: req.body.name,
+                            phone: req.body.phone,
+                            type: req.body.type
+                        });
+
+                        user
+                            .save()
+                            .then(result => {
+                                res.status(200).json({
+                                    success: true,
+                                    message: "User created successfully"
+                                })
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err,
+                                    message: "Error creating the user"
+                                })
+                            })
+                    }
+                })
+            }
+        })
+};
