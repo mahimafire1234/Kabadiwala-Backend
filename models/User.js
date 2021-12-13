44bfd8dc4d039b03be3395e14c2333c9a99070d1
rajivsunar <rajivsunar07@gmail.com>
2021-12-13 18:00:33 +0545

    User model created

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    name: {type: String},
+    phone: {type: String},
+    password: {type: String},
+    image: {type: String},
+    type: {
+        type: String,
+        enum: ['vendor', 'individual', 'normal']
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
