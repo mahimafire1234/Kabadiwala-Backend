1cd208ac7f5fca708e76814169c943b0e3c41ead
rajeesha123 <rajeesha456@gmail.com>
2021-12-14 20:14:00 +0545

    updated

--- routes/User.js
+++ routes/User.js
@@ -2,8 +2,9 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 
-const { register } = require('../controllers/User')
+const { register, get_company } = require('../controllers/User')
 
 router.post('/register', register)
+router.get('/get_company', get_company)
 
 module.exports = router
