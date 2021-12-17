10aecadcd8e127893037de44de593dee62ab207f
KarkiRajiv <rajivkarky15@gmail.com>
2021-12-17 11:35:37 +0545

    login testing done

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    // _id: mongoose.Schema.Types.ObjectId,
     email: {
         type: String,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
