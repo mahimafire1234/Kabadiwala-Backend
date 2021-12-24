8793783939d45dcfb2faad6a1942a3b3a07e3e43
rajivsunar <rajivsunar07@gmail.com>
2021-12-24 19:18:48 +0545

    User registration error corrected

--- controllers/User.js
+++ controllers/User.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv")
 const fs = require('fs')
 
-const User = require("../models/user");
+const User = require("../models/User");
 const { Module } = require("module");
 
 //register user account
