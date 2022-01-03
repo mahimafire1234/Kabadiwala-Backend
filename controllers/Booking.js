f329674f4781e48ee7d813e1f355554adb2ac347
KarkiRajiv <rajivkarky15@gmail.com>
2022-01-03 23:26:31 +0545

    completed sprint4 backend API's

--- controllers/Booking.js
+++ controllers/Booking.js
@@ -1,3 +1,4 @@
+const res = require("express/lib/response");
 const mongoose = require("mongoose");
 
 const Booking = require("../models/Booking");
@@ -21,3 +22,76 @@ exports.book = (req, res) => {
         })
     })
 }
+
+// get book request
+
+exports.getAllBooks = async function(req,res)  {
+try {
+    const _id = req.userdata._id;
+   const booking = await Booking.find({company:_id}).populate("user")
+   res.json({success:true,data:booking})
+} catch (error) {
+    res.status(500).json({
+        error: error,
+        message: "Failed to get book"
+    })
+}
+res.end()
+}
+
+
+exports.approved=async (req,res)=>{
+    try {
+        const _id =req.params._id
+        const approved_data= await Booking.updateOne(
+            {_id:_id},
+            {status:req.body.status}
+
+
+        )
+        res.json({success:true,data:approved_data})
+        
+
+        
+    } catch (error) {
+        res.status(500).json({
+            error:error,
+            message: "failed to approved"
+        })
+        
+    }
+
+
+
+}
+
+exports.getapproved = async function(req,res)  {
+    try {
+        const _id = req.userdata._id;
+
+       const approved = await Booking.find({company:_id,status:"accepted"})
+       res.json({success:true,data:approved})
+    } catch (error) {
+        res.status(500).json({
+            error: error,
+            message: "Failed to get book"
+        })
+    }
+    res.end()
+    }
+
+    exports.getdeclined = async function(req,res)  {
+        try {
+            const _id = req.userdata._id;
+    
+           const reject = await Booking.find({company:_id,status:"rejected"})
+           res.json({success:true,data:reject})
+        } catch (error) {
+            res.status(500).json({
+                error: error,
+                message: "Failed to get book"
+            })
+        }
+        res.end()
+        }
+
--- routes/Booking.js
+++ routes/Booking.js
@@ -2,11 +2,29 @@ const express = require('express')
 const User = require('../models/Booking')
 const router = new express.Router()
 
-const { book } = require('../controllers/Booking')
-const { verifyUser } = require('../middleware/auth')
+const { book,getAllBooks,approved,getapproved, getdeclined } = require('../controllers/Booking')
+const { verifyUser, verifyAdmin } = require('../middleware/auth')
 
 router
 .route('/')
 .post(verifyUser, book)
 
+router
+.route('/')
+.get(verifyAdmin, getAllBooks)
+
+
+router
+.route('/approved/:_id')
+.put(verifyAdmin, approved)
+
+
+router
+.route('/get_approved')
+.get(verifyAdmin, getapproved)
+module.exports = router
+
+router
+.route('/get_decline')
+.get(verifyAdmin, getdeclined)
 module.exports = router
