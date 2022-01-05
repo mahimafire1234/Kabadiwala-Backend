ece2b0f264cfa67a563f9996564f7c2152d0521d
Sanjila Shrestha <sanjilashrestha00@gmail.com>
2022-01-05 22:26:07 +0545

    view_appointments

--- controllers/Booking.js
+++ controllers/Booking.js
@@ -9,99 +9,54 @@ exports.book = (req, res) => {
     const booking = new Booking(req.body);
 
     booking.save()
-    .then(result => {
-        res.status(200).json({
-            success: true,
-            message: "Booking scheduled successfully"
+        .then(result => {
+            res.status(200).json({
+                success: true,
+                message: "Booking scheduled successfully"
+            })
         })
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err,
-            message: "Booking failed"
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                message: "Booking failed"
+            })
         })
-    })
 }
 
 //view bookings(appointments)
 exports.view_appointments = (req, res, next) => {
     const user_id = req.params.id;
-    console.log(user_id),
+    console.log(user_id);
 
-    Booking.find({user : user_id}).exec()
-    .then(
-        FormData => {
-         const response = {
-         count: FormData.length,
-         appointment: FormData.map(data => {
-         return {
-             status:data.status,
-             user: data.user,
-             date: data.date,
-             time: data.time,
-             location: data.location,
-             items: data.items,
-             total_price: data.total_price,
-         };
+    try {
+        Booking.find({ user: user_id }).exec()
+            .then(
+                (data) => {
+                    if (data.length > 0) {
+                        console.log(data);
+                        res.status(200).send({ data: data });
+                    }
+                    else {
+                        response.send({ success: "false", message: "You have no appointments" });
 
-     })
-     }
-     res.status(200).json(response);
-     })
-    .catch(err=>{
-        res.send({
-            error:err
-        })
-    })
- }
+                    }
+
+                }
+            )
+
+    } catch (error) {
+        response.status(404).json({ error: error })
+    }
+}
+
+
 // get book request
 
-exports.getAllBooks = async function(req,res)  {
-try {
-    const _id = req.userdata._id;
-   const booking = await Booking.find({company:_id}).populate("user")
-   res.json({success:true,data:booking})
-} catch (error) {
-    res.status(500).json({
-        error: error,
-        message: "Failed to get book"
-    })
-}
-res.end()
-}
-
-
-exports.approved=async (req,res)=>{
-    try {
-        const _id =req.params._id
-        const approved_data= await Booking.updateOne(
-            {_id:_id},
-            {status:req.body.status}
-
-
-        )
-        res.json({success:true,data:approved_data})
-        
-
-        
-    } catch (error) {
-        res.status(500).json({
-            error:error,
-            message: "failed to approved"
-        })
-        
-    }
-
-
-
-}
-
-exports.getapproved = async function(req,res)  {
+exports.getAllBooks = async function (req, res) {
     try {
         const _id = req.userdata._id;
-
-       const approved = await Booking.find({company:_id,status:"accepted"})
-       res.json({success:true,data:approved})
+        const booking = await Booking.find({ company: _id }).populate("user")
+        res.json({ success: true, data: booking })
     } catch (error) {
         res.status(500).json({
             error: error,
@@ -109,45 +64,86 @@ exports.getapproved = async function(req,res)  {
         })
     }
     res.end()
-    }
-
-    exports.getdeclined = async function(req,res)  {
-        try {
-            const _id = req.userdata._id;
-    
-           const reject = await Booking.find({company:_id,status:"rejected"})
-           res.json({success:true,data:reject})
-        } catch (error) {
-            res.status(500).json({
-                error: error,
-                message: "Failed to get book"
-            })
-        }
-        res.end()
-        }
+}
 
 
-
-
-        exports.change_status=async (req,res)=>{
+exports.approved = async (req, res) => {
     try {
-        const _id =req.params._id
-        const approved_data= await Booking.updateOne(
-            {_id:_id},
-            {status:req.body.status}
+        const _id = req.params._id
+        const approved_data = await Booking.updateOne(
+            { _id: _id },
+            { status: req.body.status }
 
 
         )
-        res.json({success:true,data:approved_data})
-        
+        res.json({ success: true, data: approved_data })
 
-        
+
+
     } catch (error) {
         res.status(500).json({
-            error:error,
+            error: error,
             message: "failed to approved"
         })
-        
+
+    }
+
+
+
+}
+
+exports.getapproved = async function (req, res) {
+    try {
+        const _id = req.userdata._id;
+
+        const approved = await Booking.find({ company: _id, status: "accepted" })
+        res.json({ success: true, data: approved })
+    } catch (error) {
+        res.status(500).json({
+            error: error,
+            message: "Failed to get book"
+        })
+    }
+    res.end()
+}
+
+exports.getdeclined = async function (req, res) {
+    try {
+        const _id = req.userdata._id;
+
+        const reject = await Booking.find({ company: _id, status: "rejected" })
+        res.json({ success: true, data: reject })
+    } catch (error) {
+        res.status(500).json({
+            error: error,
+            message: "Failed to get book"
+        })
+    }
+    res.end()
+}
+
+
+
+
+exports.change_status = async (req, res) => {
+    try {
+        const _id = req.params._id
+        const approved_data = await Booking.updateOne(
+            { _id: _id },
+            { status: req.body.status }
+
+
+        )
+        res.json({ success: true, data: approved_data })
+
+
+
+    } catch (error) {
+        res.status(500).json({
+            error: error,
+            message: "failed to approved"
+        })
+
     }
 
 
