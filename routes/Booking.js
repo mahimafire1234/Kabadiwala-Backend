60421f94283f56a0d90ba5cbe9ee970833411d2b
KarkiRajiv <rajivkarky15@gmail.com>
2022-01-08 17:01:40 +0545

    :zap: optimizing route

--- routes/Booking.js
+++ routes/Booking.js
@@ -2,29 +2,37 @@ const express = require('express')
 const User = require('../models/Booking')
 const router = new express.Router()
 
-const { book,getAllBooks,approved,getapproved, getdeclined } = require('../controllers/Booking')
+const { book,approved,declined,getapproved, getdeclined,getPending } = require('../controllers/Booking')
 const { verifyUser, verifyAdmin } = require('../middleware/auth')
 
 router
 .route('/')
 .post(verifyUser, book)
 
-router
-.route('/getAllBooks')
-.get(verifyAdmin, getAllBooks)
-
+// router
+// .route('/getAllBooks')
+// .get(verifyAdmin, getAllBooks)
 
 router
 .route('/approved/:id')
 .put(verifyAdmin, approved)
 
+router
+.route('/declined/:id')
+.put(verifyAdmin, declined)
+
+
+
 
 router
 .route('/get_approved')
 .get(verifyAdmin, getapproved)
-module.exports = router
 
 router
 .route('/get_decline')
 .get(verifyAdmin, getdeclined)
+
+router
+.route('/get_pending')
+.get(verifyAdmin, getPending)
 module.exports = router
