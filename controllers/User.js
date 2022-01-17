84bc2e45169ef4284a15c98cefd34729804b6ec2
rajivsunar <rajivsunar07@gmail.com>
2022-01-17 18:24:07 +0545

    Update profile and change password backend completed

--- controllers/User.js
+++ controllers/User.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv")
 const fs = require('fs')
-const auth= require("../middleware/auth")
+const auth = require("../middleware/auth")
 
 const User = require("../models/user");
 
@@ -21,7 +21,7 @@ exports.register = (req, res) => {
                     message: "Email address already used"
                 });
             } else {
-                
+
                 bcrypt.hash(req.body.password, 10, (err, hash) => {
                     if (err) {
                         return res.status(500).json({
@@ -29,14 +29,14 @@ exports.register = (req, res) => {
                             message: "Registration failed"
                         })
                     } else {
-                     
+
                         const user = new User({
                             _id: mongoose.Types.ObjectId(),
                             email: req.body.email,
                             password: hash,
                             name: req.body.name,
                             phone: req.body.phone,
-                            companyLocation:req.body.companyLocation,
+                            companyLocation: req.body.companyLocation,
                             usertype: req.body.usertype
                         });
 
@@ -48,7 +48,7 @@ exports.register = (req, res) => {
                                 res.status(200).json({
                                     success: true,
                                     message: "User created successfully",
-                                    data:result
+                                    data: result
                                 })
                             })
                             .catch(err => {
@@ -65,7 +65,7 @@ exports.register = (req, res) => {
 };
 
 //login to account
-exports.login_user =  function (req, res) {
+exports.login_user = function (req, res) {
     console.log(req.body)
     // first we need email and passord 
     const email = req.body.email // email should match to body of postman
@@ -101,7 +101,7 @@ exports.login_user =  function (req, res) {
                     data: userdata,
                     message: "auth success"
                 });
-                
+
 
             }
 
@@ -117,88 +117,190 @@ exports.login_user =  function (req, res) {
 
 //show list of companies
 exports.get_company = (req, res, next) => {
-    User.find({usertype:'company'}).exec()
-    .then(docs => {
-         const response = {
-         count: docs.length,
-         user: docs.map(doc => {
-         return {
-             id:doc._id,
-             email: doc.email,
-             name: doc.name,
-             phone: doc.phone,
-             image: doc.image,
-         };
-     })
-     }
-     console.log(response)
-     res.status(200).json(response);
-     })
-    .catch(err=>{
-        res.status(401).json({
-            error: err
+    User.find({ usertype: 'company' }).exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                user: docs.map(doc => {
+                    return {
+                        id: doc._id,
+                        email: doc.email,
+                        name: doc.name,
+                        phone: doc.phone,
+                        image: doc.image,
+                    };
+                })
+            }
+            console.log(response)
+            res.status(200).json(response);
         })
-    })
- }
- 
+        .catch(err => {
+            res.status(401).json({
+                error: err
+            })
+        })
+}
+
 // show one company
 exports.showOne = (req, res, next) => {
     console.log("hit")
     const id = req.params.id;
     User.findById(id).exec()
-    .then(docs => {
-        const response = {
-            user: {
+        .then(docs => {
+            const response = {
+                user: {
                     email: docs.email,
                     name: docs.name,
                     phone: docs.phone,
                     image: docs.image,
-                    id:docs._id
-            }
+                    id: docs._id
+                }
             }
             res.status(200).json(response)
-        }  
-    )
-    .catch(err=> {
-        res.status(401).json({
-            error:err
-        })
-        console.log(err);
-    });
+        }
+        )
+        .catch(err => {
+            res.status(401).json({
+                error: err
+            })
+            console.log(err);
+        });
 }
 
 //show logged in company
-exports.loggedin_company=   function(req,res){
+exports.loggedin_company = function (req, res) {
     // console.log("hit");
-    const id =req.userdata._id;
+    const id = req.userdata._id;
     // console.log(id);
-     User.findById(id)
-    .then(function (data) {
-        res
-          .status(200)
-          .json({ success: true, data });
-        // console.log("company data aayo"+{data});
-      })
-      .catch(function (e) {
-        res.status(500).json({ message: e });
-      });
-      res.end()
-    }
+    User.findById(id)
+        .then(function (data) {
+            res
+                .status(200)
+                .json({ success: true, data });
+            // console.log("company data aayo"+{data});
+        })
+        .catch(function (e) {
+            res.status(500).json({ message: e });
+        });
+    res.end()
+}
 
 
 //show logged in user
-exports.loggedin_user=  function(req,res){
+exports.loggedin_user = function (req, res) {
     // console.log("hit");
-    const id =req.userdata._id;
+    const id = req.userdata._id;
     // console.log(id);
     User.findById(id)
-    .then(function (data) {
-        res
-          .status(200)
-          .json({ success: true, data });
-        console.log("logged in user data aayo"+{data});
-      })
-      .catch(function (e) {
-        res.status(500).json({ message: e });
-      });
-    }
+        .then(function (data) {
+            res
+                .status(200)
+                .json({ success: true, data });
+            console.log("logged in user data aayo" + { data });
+        })
+        .catch(function (e) {
+            res.status(500).json({ message: e });
+        });
+}
+
+exports.get_user_from_id = (req, res, next) => {
+
+    User.findOne({ _id: req.params.id })
+        .then(result => {
+            res.status(200).json({
+                success: true,
+                result: result
+            })
+            next()
+        })
+        .catch(err => {
+            res.status(401).json({ error: err })
+        })
+
+}
+
+exports.update = (req, res, next) => {
+
+    const id = req.userdata._id;
+
+    User.findByIdAndUpdate(id, req.body)
+        .exec()
+        .then(result => {
+        
+            if(req.file != undefined){
+                User.findByIdAndUpdate(id, { image: req.file.path })
+                .then(result => {
+                    res.status(200).json({
+                        success: true,
+                        message: "User details updated"
+                    })
+                })
+                .catch(err => {
+                    res.status(401).json({
+                        success: false,
+                        error: err,
+                        message: "User image update failed"
+                    })
+                })
+            }else{
+                res.status(200).json({
+                    success: true,
+                    message: "User details updated"
+                })
+            }
+        
+        })
+        .catch(err => {
+            res.status(401).json({
+                success: false,
+                error: err,
+                message: "Error in updating user details"
+            })
+        });
+};
+
+exports.change_password = (req, res, next) => {
+    User.findById(req.userdata._id)
+        .exec()
+        .then(user => {
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: "Incorrect password"
+                    })
+                }
+
+                if (result) {
+                    bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+                        if (err) {
+                            return res.status(500).json({
+                                error: err
+                            })
+                        } else {
+
+                            User.findByIdAndUpdate(req.userdata._id, { password: hash })
+                                .then(result => {
+                                    return res.status(200).json({
+                                        success: true,
+                                        message: "Password changed successfully"
+                                    })
+                                })
+                                .catch(err => {
+                                    res.status(401).json({
+                                        error: err,
+                                        message: "Error in password change"
+                                    })
+                                })
+
+                        }
+                    })
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
+}
--- routes/User.js
+++ routes/User.js
@@ -2,9 +2,10 @@ const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
 const auth=require("../middleware/auth")
+const upload = require("../middleware/fileUpload")
 
 //require Controllers
-const { register,showOne, get_company,loggedin_company,loggedin_user} = require('../controllers/User')
+const { register,showOne, get_company,loggedin_company,loggedin_user, update, change_password} = require('../controllers/User')
 
 //link
 router.post('/register', register)
@@ -13,7 +14,13 @@ router.get('/get_company', get_company)
 router.get("/loggedin_company",auth.verifyAdmin,loggedin_company)
 router.get("/loggedin_user",auth.verifyUser,loggedin_user)
 
+router
+.route('/')
+.patch(auth.verifyLoggedIn, upload.single('image'), update)
 
+router
+.route('/password')
+.patch(auth.verifyLoggedIn, change_password)
 
 const userController= require('../controllers/User')
 
